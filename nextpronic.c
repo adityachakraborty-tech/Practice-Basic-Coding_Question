57863c2c7b1eec4dd2a0ed4db41be8045a7baa9e
Aditya Chakraborty <75158043+adityachakraborty-tech@users.noreply.github.com>
2022-06-11 23:48:59 +0530

    Day-13

--- nextpronic.c
+++ nextpronic.c
@@ -1,27 +1,26 @@
-# include <stdio.h>
-# include <stdlib.h>
-
-int main()
-{
-    int prno,i,n,flg;
-			printf("\n\n Find the Pronic Numbers between 1 to 1000: \n");
-			printf(" -----------------------------------------------\n");
-			printf(" The Pronic numbers are: \n");
- for(prno=1;prno<=1000;prno++)
- {
-     flg=0;
-    for(i=1;i<=prno;i++)
+#include<stdio.h>
+int ispronic (int n){
+    int i;
+    for(i=1;i<=n/2;i++)
+    {
+        if(i*(i+1)==n);
+        return 1;
+    }
+    return 0;
+}
+void main(){
+    int n,r;
+    printf("enter a number :  ");
+    scanf("%d",&n);
+    n=n+1;
+    while(1){
+        r=ispronic(n);
+        if(r==1)
         {
-            if(i*(i+1)==prno) 
-            {
-            flg=1;
+            printf("%d",n);
             break;
-            }
         }
-            if(flg==1)
-            {
-			printf("%d ",prno);
-            }
- }
-            printf("\n");
+   
+    }
+         n=n+1;
 }
